7f086dac1f327d2fe7f9bd7a7cd1e168569ab5c2
Muzammil Ali <muzammilali28@gmail.com>
2023-05-31 17:16:23 +0500

    Post's Auhtor Name display - Component

---
+++ src/features/posts/components/PostsAuthor.jsx
@@ -0,0 +1,15 @@
+import { useSelector } from "react-redux";
+import { selectAllUsers } from "../../users/slice/userSlice";
+
+const PostsAuthor = ({ userId }) => {
+
+  const users = useSelector(selectAllUsers);
+
+  const author = users.find((user) => user.id === userId);
+
+  return (
+    <span>by ~ {author ? author.name : "Unknow Author"}</span>
+  )
+}
+
+export default PostsAuthor
